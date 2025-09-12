e32ff8c318a030590115b3b1957506be52643014
konstin <konstin@mailbox.org>
2025-09-12 13:25:59 +0200

    More fixes

--- src/rustup/_shim.py
+++ src/rustup/_shim.py
@@ -1,15 +1,21 @@
 #!python
 
-import shutil
 import subprocess
 import sys
 import os
+from os.path import dirname, abspath, join, exists
 
 
 def _shim(proxy: str) -> None:
-    rustup = shutil.which("rustup")
-    if not rustup:
-        raise RuntimeError("Could not find rustup binary in PATH")
+    parent = dirname(abspath(__file__))
+    # Ensure `rustup` is on PATH
+    if parent not in os.environ["PATH"].split(os.pathsep):
+        os.environ["PATH"] = os.pathsep.join(
+            [parent, *os.environ["PATH"].split(os.pathsep)]
+        )
+    rustup = join(parent, "rustup")
+    if not exists(rustup):
+        raise RuntimeError(f"rustup binary does not exist: {rustup}")
     # Keep pretending we're the shim binary
     args = (proxy, *sys.argv[1:])
 
